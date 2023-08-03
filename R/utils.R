3e63a7c1ae19cd0aa38c0f243211541d2c2ec49f
Quanhu Sheng <shengqh@gmail.com>
2023-08-02 21:34:47 -0500

    bugfix

--- R/utils.R
+++ R/utils.R
@@ -102,7 +102,7 @@ my_em<-function(values, data_name="em", D1="normal", D2="normal", t=1e-64, cutof
 
 # ----
 
-my_cutoff<-function (my_out, t = 1e-64, nb = 10, distr = 2, type1 = 0.05, level = 0.95) 
+my_cutoff<-function (tagname, my_out, t = 1e-64, nb = 10, distr = 2, type1 = 0.05, level = 0.95) 
 {
   coef <- my_out$out@coef
   the_names <- names(coef)
@@ -113,10 +113,10 @@ my_cutoff<-function (my_out, t = 1e-64, nb = 10, distr = 2, type1 = 0.05, level 
     return(as.list(x))
   })
 
-  message(paste0(tagname, " choisycutoff:::lci0 start ..."))
+  #message(paste0(tagname, " choisycutoff:::lci0 start ..."))
   out <- sapply(coef, function(x) choisycutoff:::lci0(x, mean(my_out$lambda), 
                                                       choisycutoff:::hash[[my_out$D1]], choisycutoff:::hash[[my_out$D2]], my_out$data, t))
-  message(paste0(tagname, " choisycutoff:::lci0 finished ..."))
+  #message(paste0(tagname, " choisycutoff:::lci0 finished ..."))
 
   lambda <- rnorm(nb, out[1, ], out[2, ])
   coef <- sapply(coef, function(x) unlist(x))
@@ -127,11 +127,14 @@ my_cutoff<-function (my_out, t = 1e-64, nb = 10, distr = 2, type1 = 0.05, level 
     return(as.list(x))
   })
 
-  message(paste0(tagname, " choisycutoff:::cutoff0 start ..."))
+  #message(paste0(tagname, " choisycutoff:::cutoff0 start ..."))
   out <- sapply(coef, function(x) with(x, choisycutoff:::cutoff0(mu1, sigma1, mu2, sigma2, lambda, my_out$D1, my_out$D2, distr, type1)))
-  message(paste0(tagname, " choisycutoff:::cutoff0 finished ..."))
+  #message(paste0(tagname, " choisycutoff:::cutoff0 finished ..."))
 
+  #message(paste0(tagname, " MASS::fitdistr start ..."))
   out <- MASS::fitdistr(out, "normal")
+  #message(paste0(tagname, " MASS::fitdistr finished"))
+
   the_mean <- out$estimate["mean"]
   level <- (1 - level)/2
   level <- c(level, 1 - level)
@@ -144,9 +147,9 @@ my_cutoff<-function (my_out, t = 1e-64, nb = 10, distr = 2, type1 = 0.05, level 
 # ----
 
 get_cutoff<-function(tagname, values, prefix=NULL, cur_startval=0){
-  message(paste0(tagname, " my_em start ..."))
+  #message(paste0(tagname, " my_em start ..."))
   my_out <- my_em(values,"normal","normal", cutoff_point=cur_startval)
-  message(paste0(tagname, " my_em finished"))
+  #message(paste0(tagname, " my_em finished"))
 
   coef = my_out$out@coef
   
@@ -166,12 +169,12 @@ get_cutoff<-function(tagname, values, prefix=NULL, cur_startval=0){
   }
   
   cut_off <- tryCatch({
-      my_cutoff(my_out)
+      my_cutoff(tagname, my_out)
     }, error=function(e){
       if(cur_startval == 0){
         stop(paste0(tagname, " failed failed: ", e))
       }else{
-        warning(paste0(tagname, " failed failed: ", e, ", use start value ", cur_startval, " as cutoff."))
+        message(paste0(tagname, " failed failed: ", e, ", use start value ", cur_startval, " as cutoff."))
         return(cur_startval)
       }
     }
